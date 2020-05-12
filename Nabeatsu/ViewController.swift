9ba372abc0fe10e9ee500c67ab25edf2a5f3409d
maya <amacrk@i.softbank.jp>
2020-05-12 20:27:15 +0900

    multiple of 5 and prime number

--- Nabeatsu/ViewController.swift
+++ Nabeatsu/ViewController.swift
@@ -19,46 +19,64 @@ class ViewController: UIViewController {
         
     }
     
-    func isAho() -> Bool {
+    func isAho() -> Int {
         
         if number % 7 != 0 {
-            // 問題1: 3の倍数かどうか調べる
-            if number % 3 == 0 {
-                return true //アホになる
-            }
-            // 問題2: 1の位が3かどうか調べる
-            // if number % 10 == 3 {
-            //  return true // アホになる
-            
-            //}
-            // 問題3: 10の位が3かどうか調べる
-            // if number / 10 % 10 == 3 {
-            
-            //  return true //アホになる
-            //}
-            
-            // 問題4: 3がつくかどうか調べる
-            // 問題4をやるときは問題3と問題2の答えを消してから書こう
-            var checkNum: Int = number
-            
-            while checkNum != 0 {
-                if checkNum % 10 == 3 {
-                    return true // アホになる
-                } else {
-                    checkNum = checkNum / 10
+            if number % 5 == 0 {
+                return 2 // 犬
+            } else {
+                // 問題1: 3の倍数かどうか調べる
+                if number % 3 == 0 {
+                    return 1 //アホになる
+                }
+                // 問題2: 1の位が3かどうか調べる
+                // if number % 10 == 3 {
+                //  return true // アホになる
+                
+                //}
+                // 問題3: 10の位が3かどうか調べる
+                // if number / 10 % 10 == 3 {
+                
+                //  return true //アホになる
+                //}
+                
+                // 問題4: 3がつくかどうか調べる
+                // 問題4をやるときは問題3と問題2の答えを消してから書こう
+                var checkNum: Int = number
+                
+                while checkNum != 0 {
+                    if checkNum % 10 == 3 {
+                        return 1 // アホになる
+                    } else {
+                        checkNum = checkNum / 10
+                    }
+                }
+                
+                var n = 0
+                for i in 1..<number+1 {
+                    if number % i == 0 {
+                        n += 1
+                    }
+                }
+                if number != 1 && n <= 2 {
+                    return 3
                 }
             }
         }
         
-        return false
+        return 0
     }
     @IBAction func plusButton(){
         number = number + 1
         countLabel.text = String(number)
         
-        if isAho() == true {
+        if isAho() == 1 {
             
             faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        } else if isAho() == 2 {
+            faceLabel.text = "∪・ω・∪"
+        } else if isAho() == 3{
+            faceLabel.text = "（｀・ω・´）"
         } else {
             
             faceLabel.text = "(゜o゜)"
